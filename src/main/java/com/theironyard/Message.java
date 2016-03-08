ca75c8dd053d0191cbca45e30471ba644b156da0
ericweidman <ericweidman@gmail.com>
2016-03-08 13:51:49 -0500

    Added all needed routes

--- src/main/java/com/theironyard/Message.java
+++ src/main/java/com/theironyard/Message.java
@@ -1,14 +1,24 @@
 package com.theironyard;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * Created by ericweidman on 3/7/16.
  */
+@Entity
 public class Message {
+    @Id
+    @GeneratedValue
     int id;
     String message;
 
-    public Message( String message) {
-        this.id = id;
+    public Message() {
+    }
+
+    public Message(String message) {
         this.message = message;
     }
 }
--- src/main/java/com/theironyard/MicroblogController.java
+++ src/main/java/com/theironyard/MicroblogController.java
@@ -1,6 +1,7 @@
 package com.theironyard;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,13 +15,13 @@ import java.util.ArrayList;
  */
 @Controller
 public class MicroblogController {
-
-    ArrayList<Message> messages = new ArrayList<Message>();
+    @Autowired
+    MessageRepository message;
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String home(Model model, HttpSession session){
         model.addAttribute("userName", session.getAttribute("userName"));
-        model.addAttribute("message", messages);
+        model.addAttribute("message", message.findAll());
         return "home";
     }
     @RequestMapping(path = "/login", method = RequestMethod.POST)
@@ -31,18 +32,20 @@ public class MicroblogController {
         }
         return "redirect:/";
     }
-    @RequestMapping(path = "/message", method = RequestMethod.POST)
+    @RequestMapping(path = "/add-message", method = RequestMethod.POST)
     public String message(HttpSession session, String message) {
         session.setAttribute("messages", session.getAttribute("messages"));
-        Message message1 = new Message(message);
-        messages.add(message1);
 
         return "redirect:/";
     }
-    @RequestMapping(path = "/delete", method = RequestMethod.POST)
+    @RequestMapping(path = "/delete-message", method = RequestMethod.POST)
     public String delete(HttpSession session, int id){
         session.setAttribute("delete", id);
-        messages.remove(messages.size() - 1);
+        return "redirect:/";
+    }
+    @RequestMapping(path = "update-message", method = RequestMethod.POST)
+    public String updateMessage(HttpSession session){
+
         return "redirect:/";
     }
 }
